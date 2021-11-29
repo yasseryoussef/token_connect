8bdfc2f67ce48e8d153eece67a879221c5f9459d
yaser <swap-solutions@hotmail.com>
2021-11-29 19:32:05 +0200

    Add project files.

---
+++ roken_connect/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿
+namespace roken_connect
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_pin = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txt_pin
+            // 
+            this.txt_pin.Location = new System.Drawing.Point(244, 51);
+            this.txt_pin.Name = "txt_pin";
+            this.txt_pin.Size = new System.Drawing.Size(139, 20);
+            this.txt_pin.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(286, 100);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "اتصال";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(389, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "رمز الحماية ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(514, 265);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txt_pin);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_pin;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ roken_connect/Form1.cs
@@ -0,0 +1,99 @@
+﻿using Net.Pkcs11Interop.Common;
+using Net.Pkcs11Interop.HighLevelAPI;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace roken_connect
+{
+    public partial class Form1 : Form
+    {
+        string DllLibPath = "eps2003csp11.dll";
+        string tokentype = "Egypt Trust Sealing CA";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Pkcs11InteropFactories factories = new Pkcs11InteropFactories();
+            using (IPkcs11Library pkcs11Library = factories.Pkcs11LibraryFactory.LoadPkcs11Library(factories, DllLibPath, AppType.MultiThreaded))
+            {
+                ISlot slot = pkcs11Library.GetSlotList(SlotsType.WithTokenPresent).FirstOrDefault();
+
+                if (slot is null)
+                {
+                    MessageBox.Show("No slots found");
+                    return;
+                }
+
+                ITokenInfo tokenInfo = slot.GetTokenInfo();
+
+                ISlotInfo slotInfo = slot.GetSlotInfo();
+
+
+                using (var session = slot.OpenSession(SessionType.ReadWrite))
+                {
+                    try
+                    {
+                        session.Login(CKU.CKU_USER, Encoding.UTF8.GetBytes(txt_pin.Text));
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("pin is not correct");
+                        return;
+                    }
+
+                    var certificateSearchAttributes = new List<IObjectAttribute>()
+                    {
+                        session.Factories.ObjectAttributeFactory.Create(CKA.CKA_CLASS, CKO.CKO_CERTIFICATE),
+                        session.Factories.ObjectAttributeFactory.Create(CKA.CKA_TOKEN, true),
+                        session.Factories.ObjectAttributeFactory.Create(CKA.CKA_CERTIFICATE_TYPE, CKC.CKC_X_509)
+                    };
+
+                    IObjectHandle certificate = session.FindAllObjects(certificateSearchAttributes).FirstOrDefault();
+
+                    if (certificate is null)
+                    {
+                        MessageBox.Show("Certificate not found");
+                        return;
+
+                    }
+
+                    X509Store store = new X509Store(StoreName.My, StoreLocation.CurrentUser);
+                    store.Open(OpenFlags.MaxAllowed);
+
+                    // find cert by thumbprint
+                    var foundCerts = store.Certificates.Find(X509FindType.FindByIssuerName, tokentype, false);
+
+                    //var foundCerts = store.Certificates.Find(X509FindType.FindBySerialNumber, "2b1cdda84ace68813284519b5fb540c2", true);
+
+
+
+                    if (foundCerts.Count == 0)
+                    {
+                        MessageBox.Show("no device detected");
+                        return;
+                    }
+
+
+                    var certForSigning = foundCerts[0];
+                    store.Close();
+
+
+                }
+            }
+
+            MessageBox.Show("connect ");
+            this.Hide();
+        }
+    }
+}
